ebaebf46c12cae30397ef4e7559551dedec16983
Kazuki Matsuda <mats.kazuki@gmail.com>
2023-10-10 08:13:45 +0000

    dynamodb にセッション情報を保存するように変更

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import json
 import os
+import uuid
 import boto3
+from pynamodb.models import Model
+from pynamodb.attributes import UnicodeAttribute, JSONAttribute, MapAttribute, ListAttribute
 import streamlit as st
 
 AWS_REGION = os.environ.get('AWS_REGION', 'us-west-2')
@@ -8,54 +11,76 @@ AWS_REGION = os.environ.get('AWS_REGION', 'us-west-2')
 # Bedrock Client
 bedrock = boto3.client(service_name='bedrock-runtime', region_name=AWS_REGION)
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [
-        {"role": "system", "content": ""}
-    ]
+class Message(MapAttribute):
+    role = UnicodeAttribute()
+    content = UnicodeAttribute()
 
+class SesstionModel(Model):
+    class Meta:
+        table_name = 'ChatHostory'
+        #host = 'http://localhost:8000'
+        region = AWS_REGION
+    sesstion_id = UnicodeAttribute(hash_key=True)
+    messages = ListAttribute(of=Message)
+
+#if not "AWS_EXECUTION_ENV" in os.environ:
+#    ChatHistory.create_table(read_capacity_units=1, write_capacity_units=1)
+
+if 'session_id' not in st.session_state:
+    session_id = str(uuid.uuid4())
+    st.session_state['session_id'] = session_id
+    SesstionModel(session_id, messages=[]).save()
 
 # チャットボットとやりとりする関数
 def communicate():
-    messages = st.session_state["messages"]
+    session_id = st.session_state['session_id']
+    session = SesstionModel.get(session_id)
 
-    user_message = {"role": "user", "content": st.session_state["user_input"]}
-    messages.append(user_message)
+    user_message = Message(role='Human', content=st.session_state['user_input'])
+    session.messages.append(user_message)
 
-    prompt = "Human:" + "\n\n".join(f"{message['role']}: {message['content']}" for message in messages[1:]) + "\n\nAssistant:"
+    prompt = '\n\n'.join([f"{msg['role']}: {msg['content']}" for msg in session.messages]) + '\n\nAssistant:'
 
     body = json.dumps({
-        "prompt": prompt,
-        "max_tokens_to_sample": 300,
-        "temperature": 0.1,
-        "top_p": 0.9,
+        'prompt': prompt,
+        'max_tokens_to_sample': 300,
+        'temperature': 0.1,
+        'top_p': 0.9,
     })
 
-    modelId = 'anthropic.claude-v2'
-    accept = 'application/json'
-    contentType = 'application/json'
-
-    response = bedrock.invoke_model(body=body, modelId=modelId, accept=accept, contentType=contentType)
+    response = bedrock.invoke_model(
+        modelId='anthropic.claude-v2',
+        accept='application/json',
+        contentType='application/json',
+        body=body,
+    )
     response_body = json.loads(response.get('body').read())
 
     bot_message_content = response_body.get('completion')
-    bot_message = {"role": "assistant", "content": bot_message_content}
-    messages.append(bot_message)
 
-    st.session_state["user_input"] = ""  # 入力欄を消去
+    bot_message = Message(role='Assistant', content=bot_message_content)
+    session.messages.append(bot_message)
+
+    # DynamoDB に保存
+    session.save()
+
+    # 入力欄を消去
+    st.session_state['user_input'] = ''
 
 
 # ユーザーインターフェイスの構築
-st.title("Bedrock Chatbot")
-st.write("Bedrock and Claudeモデルを使ったチャットボットです。")
+st.title('[Demo] Bedrock Chat')
+st.write('Bedrock と Streamlit を利用したチャットアプリです。')
+st.write('Session ID: ' + st.session_state['session_id'])
 
-user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
+user_input = st.text_input('メッセージを入力してください。', key='user_input', on_change=communicate)
 
-if st.session_state["messages"]:
-    messages = st.session_state["messages"]
-
-    for message in reversed(messages[1:]):  # 直近のメッセージを上に
+if 'session_id' in st.session_state:
+    session_id = st.session_state['session_id']
+    session = SesstionModel.get(session_id)
+    for msg in reversed(session.messages):  # 直近のメッセージを上に
         speaker = "🙂"
-        if message["role"] == "assistant":
+        if msg['role'] == 'Assistant':
             speaker = "🤖"
 
-        st.write(speaker + ": " + message["content"])
+        st.write(speaker + ': ' + msg['content'])
